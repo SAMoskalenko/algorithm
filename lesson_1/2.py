84e326b9d031e993d4b372474313c6d8196c8f28
SAMoskalenko <41867418+SAMoskalenko@users.noreply.github.com>
2019-05-18 12:11:41 +0300

    ДЗ 1

--- lesson_1/2.py
+++ lesson_1/2.py
@@ -1,2 +1,13 @@
 # Выполнить логические побитовые операции «И», «ИЛИ» и др. над числами 5 и 6.
 # Выполнить над числом 5 побитовый сдвиг вправо и влево на два знака. Объяснить полученный результат.
+
+first_number = 5
+second_number = 6
+
+print(f'Побитовое "И" {first_number & second_number}')
+print(f'Побитовое "ИЛИ" {first_number | second_number}')
+print(f'Побитовое "ИСКЛЮЧАЮЩЕЕ ИЛИ" {first_number ^ second_number}')
+print(f'Побитовое "ОТРИЦАНИЕ ПЯТЕРКИ" {~ first_number}')
+print(f'Побитовое "ОТРИЦАНИЕ ШЕСТЕРКИ" {~ second_number}')
+print(f'Побитовое "И" {first_number << 2}')
+print(f'Побитовое "И" {first_number >> 2}')
--- lesson_1/5.py
+++ lesson_1/5.py
@@ -1,1 +1,17 @@
-# Пользователь вводит две буквы.Определить, на каких местах алфавита они стоят и сколько между ними находится букв.
+# Пользователь вводит две буквы. Определить, на каких местах алфавита они стоят и сколько между ними находится букв.
+
+first_letter = ord(input('Введите первую букву: '))
+second_letter = ord(input('Введите вторую букву: '))
+
+a = ord('a')
+
+if first_letter > second_letter:
+    low = second_letter
+    high = first_letter
+else:
+    low = first_letter
+    high = second_letter
+
+print(f'{chr(first_letter)} стоит на {first_letter - a + 1} месте, \n'
+      f'{chr(second_letter)} стоит на {second_letter - a + 1} месте \n'
+      f'между ними {high - low} букв')
